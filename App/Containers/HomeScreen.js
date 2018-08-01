4bcffcaf0e10c81ba39f3628eb61a24dc5f6f5ad
Claire Ryberg <csr6ad@virginia.edu>
2018-08-01 15:36:53 +0200

    Add a homepage with navigation

---
+++ App/Containers/HomeScreen.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { ScrollView, Text, KeyboardAvoidingView, Button } from 'react-native'
+import { connect } from 'react-redux'
+// Add Actions - replace 'Your' with whatever your reducer is called :)
+// import YourActions from '../Redux/YourRedux'
+
+// Styles
+import styles from './Styles/HomeScreenStyle'
+
+class HomeScreen extends Component {
+  render () {
+    return (
+      <ScrollView style={styles.container}>
+        <KeyboardAvoidingView behavior='position'>
+          <Button 
+            onPress={() => this.props.navigation.navigate('LaunchScreen')}
+            title="Demo Launch Screen"/>
+          <Button 
+            onPress={() => this.props.navigation.navigate('TextablesScreen')}
+            title="Go to Textables Example"/>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen)
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import { StackNavigator } from 'react-navigation'
+import HomeScreen from '../Containers/HomeScreen'
 import TextablesScreen from '../Containers/TextablesScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
@@ -6,12 +7,13 @@ import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  HomeScreen: { screen: HomeScreen },
   TextablesScreen: { screen: TextablesScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
-  headerMode: 'none',
-  initialRouteName: 'TextablesScreen',
+  // headerMode: 'none',
+  initialRouteName: 'HomeScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
